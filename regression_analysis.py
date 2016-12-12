7b617e72d4c8277e451b2293d9fd0afb56944c7d
mmillervedam <mmillervedam@berkeley.edu>
2016-12-12 11:53:18 -0500

    untested analysis part 1

--- regression_analysis.py
+++ regression_analysis.py
@@ -13,8 +13,42 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
-# fxn to pull monthly numbers from postgres
+# fxn to pull monthly weather and
+def get_monthly_data():
+    """
+    This function connects to the postgres DB to pull weather data
+    corresponding to each solar location and month.
+    """
+    # Set up connection to postgres tables
+    db_loc = 'postgresql+psycopg2://teamsunshinedemo:oscarisawesome123'
+    db_loc += '@teamsunshinedemo.coga7nzsvf0h.us-east-1.rds.amazonaws.com:'
+    db_loc += '5432/solarenergy'
+    engine = create_engine(db_loc)
 
+    # get weather data
+    query = """
+            SELECT solar_fields.loc_id, solar_fields.wban_id, full_date, mwh,
+            CAST(solar_radiation as decimal) as solar_radiation,
+            FROM(
+                (SELECT loc_id, closest_station, full_date,
+                CAST(mwh as decimal) AS mwh,
+                FROM generation JOIN closest_station
+                ON generation.loc_id=closest_station.loc_id)
+                ) AS solar_fields
+            LEFT JOIN uscrn_monthly
+            ON solar_fields.wban_id=uscrn_monthly.wban_id
+            AND solar_fields.full_date=uscrn_monthly.month
+            """
+    data_df = pd.read_sql(query, engine, index_col = 'loc_id')
+
+    return data_df
+
+def run_regression(data_df):
+    """
+    Function to run a regression on the relationship between solar radiation
+    levels and energy production from solar field locations.
+    """
+    pass
 # regression1 (predict monthly solar rad from weather)
 # regression2 (predict monthly solar rad from partial)
 
