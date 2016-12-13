37c3163b9eb17e7a62ede0f81877d316797313d1
mmillervedam <mmillervedam@berkeley.edu>
2016-12-13 00:57:47 +0000

    analysis file debugged

--- energy_analysis.py
+++ energy_analysis.py
@@ -76,7 +76,7 @@ def predict_monthly_mwh(clean_df):
     """
     # remove nans
     missing = np.isnan(clean_df.solar_radiation)
-    monthly_pred_df = clean_df
+    monthly_pred_df = clean_df.copy()
     monthly_pred_df = monthly_pred_df[~missing]
 
     # selecting cols for regression
